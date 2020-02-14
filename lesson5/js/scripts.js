68b63e6c9922c21d4fd71bf9687eec3f7156b9f0
N-Class <oscar98c@gmail.com>
2020-02-14 12:45:26 -0500

    5

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -4,3 +4,23 @@ hambutton.addEventListener("click", toggleMenu, false);
 function toggleMenu() {
   document.querySelector(".navigation").classList.toggle("responsive");
 }
+
+WebFont.load({
+
+  google: {
+    families: [""]
+  }
+
+});
+
+var Now = new Date();
+var CurrentDay = Now.getDay();
+if (CurrentDay == 0) {
+  $('#colord').show()
+} else if (CurrentDay !== 1) {
+  $('#colord').show();
+} else if (CurrentDay !== 2) {
+  $('#colord').show();
+} else if (CurrentDay !== 3) {
+  $('#colord').show();
+}
