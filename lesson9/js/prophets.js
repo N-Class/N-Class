51d493037294d79cd96c2049b6fae60e5d62eb0e
N-Class <oscar98c@gmail.com>
2020-03-05 22:48:45 -0500

    prophet

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,32 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
+
+        for (let i = 0; i < prophets.length; i++) {
+
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let div1 = document.createElement('div');
+            let div2 = document.createElement('div');
+            let image = document.createElement('img');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            div1.textContent = 'Date Born: ' + prophets[i].birthdate;
+            div2.textContent = 'Born in: ' + prophets[i].birthplace;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order)
+
+            card.appendChild(h2);
+            card.appendChild(div1);
+            card.appendChild(div2);
+            card.appendChild(image);
+
+            document.querySelector('div.cards').appendChild(card);
+        }
+    });
